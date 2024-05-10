c2080bae6a312d0bee0f2fb4d14f0c1cb3b52e58
Monesha <moneshadhali@gmail.com>
2024-05-10 17:57:02 +0100

    ParkCard info displayed

--- src/components/ParkCards.js
+++ src/components/ParkCards.js
@@ -1,7 +1,18 @@
 const ParkCards = ({ selectedParks }) => {
+  const card = selectedParks.data?.map((items) => {
+    items.parks.map((park, index) => {
+      return (
+        <div className="card" key={index}>
+          <p>{park.url}</p>
+          <p>{park.fullName}</p>
+          <p>{park.designation}</p>
+        </div>
+      );
+    });
+  });
   return (
     <>
-      <section className="cardContainer"></section>
+      <section className="cardContainer">{card}</section>
     </>
   );
 };
